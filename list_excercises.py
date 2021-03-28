fd148cba028f196b908bbc1ae995379f2ed6a255
Gaming PC <sdf@gmail.com>
2021-03-28 14:06:17 +1000

    List operations and indaequate security checker to be done

--- list_excercises.py
+++ list_excercises.py
@@ -4,4 +4,7 @@ for i in range 5:
     number = int(input("Number: "))
     numbers = (*numbers, other)
 
-Print
+print("The first number is", numbers[0])
+print("The last number is", numbers[-1])
+print("The smallest number is", numbers[20])
+print("The average of the numbers is 6.2", numbers[6.2])
