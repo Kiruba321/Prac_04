250c756d953889d8392c356afc6d9aaf4877d03b
Gaming PC <sdf@gmail.com>
2021-03-28 14:18:11 +1000

    List operations and inadequate security checker to be done

--- list_excercises.py
+++ list_excercises.py
@@ -1,10 +1,11 @@
 #Basic list operations
 numbers = []
-for i in range 5:
+for i in range(5):
     number = int(input("Number: "))
-    numbers = (*numbers, other)
+    numbers.append(number)
 
 print("The first number is", numbers[0])
 print("The last number is", numbers[-1])
-print("The smallest number is", numbers[20])
-print("The average of the numbers is 6.2", numbers[6.2])
+print("The smallest number is", min(numbers))
+print("The largest number is", max(numbers))
+print("The average of the numbers is", sum(numbers)/len(numbers))
